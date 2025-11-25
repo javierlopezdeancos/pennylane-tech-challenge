fa32c07b07fea3f7b99f9aca73d858fb0c5f8128
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2025-11-25 22:12:39 +0100

    feat: add page layout wrapping all view pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,18 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import InvoicesList from './components/InvoicesList'
 import InvoiceShow from './components/InvoiceShow'
+import PageLayout from './components/PageLayout'
 
 // import GettingStarted from './GettingStarted'
 
 function App() {
   return (
     <div className="px-5">
-      {/*      <GettingStarted /> */}
       <Router>
-        <Routes>
-          <Route path="/invoice/:id" Component={InvoiceShow} />
-          <Route path="/" Component={InvoicesList} />
-        </Routes>
+        <PageLayout>
+          <Routes>
+            <Route path="/invoice/:id" Component={InvoiceShow} />
+            <Route path="/" Component={InvoicesList} />
+          </Routes>
+        </PageLayout>
       </Router>
     </div>
   )
