239070d4ea14db3e7a30625167dced386adf1c74
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2025-11-25 22:12:19 +0100

    feat: add page layout to wrapp all view pages

---
+++ src/app/components/PageLayout/index.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react'
+import { Container, Navbar, Nav } from 'react-bootstrap'
+import Logo from '../Logo'
+
+const PageLayout: React.FC<{ children?: ReactNode }> = ({ children }) => {
+  return (
+    <div>
+      <header>
+        <Navbar bg="white" expand="lg">
+          <Container className="py-2">
+            <Navbar.Brand href="/">
+              <div style={{ display: 'flex', alignItems: 'center', gap: 12 }}>
+                <Logo width={30} />{' '}
+                <span className="fs-2 fw-bolder" style={{ color: '#006667' }}>
+                  invoiceditor
+                </span>
+              </div>
+            </Navbar.Brand>
+            <Nav className="ms-auto" />
+          </Container>
+        </Navbar>
+      </header>
+      <main>{children}</main>
+    </div>
+  )
+}
+
+export default PageLayout
