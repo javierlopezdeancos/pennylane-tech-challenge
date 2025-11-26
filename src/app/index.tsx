779553e8564757fa51dcf298cbf73af695dfc572
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2025-11-26 21:49:16 +0100

    chore: import custom boostrap theme

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import { ApiProvider } from '../api'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './custom-theme.css'
 
 const domRoot = document.getElementById('root')
 const root = createRoot(domRoot!)
